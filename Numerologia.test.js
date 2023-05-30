d80b790e8287d025f8ecb8117a733d9b5065a6c2
Pedro Vieira <49994427+pedrovieira31@users.noreply.github.com>
2023-05-30 19:59:05 -0300

    Update Numerologia.test.js

--- Numerologia.test.js
+++ Numerologia.test.js
@@ -1,7 +1,7 @@
 const Numerologia = require("./Numerologia");
 
 test("class is defined", () => {
-  const expectedString = "Hello work=ld";
+  const expectedString = "Hello work=l";
   const numerology = new Numerologia();
   const value = numerology.tanga();
   expect(value).toBe(expectedString);
